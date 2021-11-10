48733c0219ed9d2349a6448a1a0086991076ed7a
Celina Hu <celinahu@10-16-10-80.dynapool.wireless.nyu.edu>
2021-11-10 04:50:09 -0500

    fick this

--- src/db.js
+++ src/db.js
@@ -22,5 +22,6 @@ const BucketList = new mongoose.Schema({
 mongoose.model('User', User);
 mongoose.model('Activity', Activity);
 mongoose.model('BucketList', BucketList);
-mongoose.connect('mongodb://localhost/ait_final');
+//mongoose.connect('mongodb://localhost/ait_final');
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true}).then((x) => console.log('Connected to the DB').catch(err => console.error('Error while connecting to DB', err)));
+module.exports = {mongoose};
