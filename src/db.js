568fb2d7ed7e5ddf433a982a66fca2dfe4e0e455
Celina Hu <celinahu@Celinas-MacBook-Pro.local>
2021-11-30 16:30:40 -0500

    working on bootstrap

--- src/db.js
+++ src/db.js
@@ -23,6 +23,6 @@ const BucketList = new mongoose.Schema({
 mongoose.model('User', User);
 //mongoose.model('Activity', Activity);
 mongoose.model('BucketList', BucketList);
-mongoose.connect('mongodb://localhost/ait_final');
-//mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true}).then((x) => console.log('Connected to the DB').catch(err => console.error('Error while connecting to DB', err)));
+//mongoose.connect('mongodb://localhost/ait_final');
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true}).then((x) => console.log('Connected to the DB').catch(err => console.error('Error while connecting to DB', err)));
 module.exports = {mongoose};
